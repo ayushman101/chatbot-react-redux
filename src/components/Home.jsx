98ab835b5df5bd876e42035d54829211ebd386ab
Ayushman Singh <amansinghayush@gmail.com>
2023-10-24 01:43:05 +0530

    home component added

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css'
+
+const Home=()=>{
+return (
+	<div className="home-container">
+		<h1 className="title"> Welcome to Student Info System</h1>
+		<button className="enroll-btn">Enroll Now!</button>
+	</div>
+)
+}
+
+export default Home;
