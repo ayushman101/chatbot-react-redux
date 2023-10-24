e2f19758200c9a162914d2bf85587579b29dc824
Ayushman Singh <amansinghayush@gmail.com>
2023-10-24 17:05:06 +0530

    redux store added

---
+++ src/components/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import enrollmentReducer from './enrollmentSlice';
+
+const store = configureStore({
+  reducer: {
+    enrollment: enrollmentReducer,
+  },
+});
+
+export default store;
+~
+
